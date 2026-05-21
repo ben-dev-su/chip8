677e79aa3b9ea52536d68d06c99e933975acf6e1
ben-dev-su <206861590+ben-dev-su@users.noreply.github.com>
2026-05-21 21:13:55 +0200

    feat(chip8): implement 0xDXYN display opcode and debug macro
    Added DEBUG preprocessor macro to better control the console logging.
    This is also in preparation for the upcoming basic renderer.
    
    Implemented CHIP-8 0xDXYN opcode to handle screen drawing.
    This one was kind of tricky, ngl. First I had to figure out how to translate
    a 2D grid to 1D array, which wasn't crazy,
    but I got stuck on defining the correct starting coordinates, where I want to
    actually start drawing. Second, I had to figure out which
    bit in the sprite sequence was set and which ones were not set.
    I had a basic idea, similar to how i extracted the first nibble from the
    opcode to use in the switch statement,
    but basically I needed to use bitwise AND masking to isolate and test all
    bits within the sprite.
    
    As a reminder:
    
    0 0 = 0
    1 0 = 0
    0 1 = 0
    1 1 = 1
    
    I'm still not sure my implementation is 100% correct (pretty sure it
    isn't), but I'll find out when this thing explodes on me. 🥴

--- src/chip8.c
+++ src/chip8.c
@@ -117,55 +117,105 @@ void decode(chip8_t *chip8) {
   unsigned short index_x;
   // Index of a v register
   unsigned short index_y;
+
+#ifdef DEBUG
   printf("executing opcode: 0x%x\n", chip8->opcode);
+#endif
 
   switch (chip8->opcode & 0xF000) {
   case 0x0000:
     op = chip8->opcode & 0x00FF;
     switch (op) {
     case 0x00E0:
+#ifdef DEBUG
       printf("clear screen\n");
+#endif
+      memset(chip8->gfx, 0, sizeof(chip8->gfx));
+      printf("\033[2J");
+      printf("\033[H");
+      fflush(stdout);
       chip8->pc += 2;
       break;
     case 0x00EE:
+#ifdef DEBUG
       printf("returns from subroutine\n");
+#endif
       chip8->pc += 2;
       break;
     }
     break;
   case 0x1000:
     chip8->pc = chip8->opcode & 0x0FFF;
+#ifdef DEBUG
     printf("jump to address: 0x%04x\n", chip8->opcode & 0x0FFF);
     printf("pc: 0x%04x\n", chip8->pc);
+#endif
     break;
   case 0x6000:
     index_x = getIndexX(chip8->opcode);
     NN = getNN(chip8->opcode);
+#ifdef DEBUG
     printf("set register V0x%x to NN: 0x%x\n", index_x, NN);
+#endif
     chip8->v[index_x] = NN;
     chip8->pc += 2;
     break;
   case 0x7000:
     index_x = getIndexX(chip8->opcode);
     NN = getNN(chip8->opcode);
+
+#ifdef DEBUG
     printf("add 0x%x to register V%x \n", NN, index_x);
+#endif
     chip8->v[index_x] += NN;
     chip8->pc += 2;
     break;
   case 0xA000:
     chip8->I = chip8->opcode & 0x0FFF;
+
+#ifdef DEBUG
     printf("set index register I to NNN: 0x%x\n", chip8->opcode & 0x0FFF);
+#endif
     chip8->pc += 2;
     break;
   case 0xD000:
-    printf("display/draw");
+
+#ifdef DEBUG
+    printf("display/draw\n");
+#endif
+    unsigned char vx = getIndexX(chip8->opcode);
+    unsigned char vy = getIndexY(chip8->opcode);
+    unsigned char xpos = chip8->v[vx];
+    unsigned char ypos = chip8->v[vy];
+    unsigned char height = getN(chip8->opcode);
+    unsigned char sprite;
+    chip8->v[0xf] = 0;
+    for (size_t row = 0; row < height; row++) {
+      sprite = chip8->memory[chip8->I + row];
+      for (size_t col = 0; col < 8; col++) {
+        if (sprite & (0b10000000 >> col)) {
+          // start index: width * y + x
+          // index pos: row * width + col
+          // = width * y + x + row * width + col
+          if (chip8->gfx[(64 * (row + ypos)) + col + xpos] == 1) {
+            chip8->v[0xf] = 1;
+          } else {
+            chip8->v[0xf] = 0;
+          }
+          chip8->gfx[(64 * (row + ypos)) + col + xpos] ^= 1;
+        }
+      }
+    }
     chip8->pc += 2;
     break;
   default:
+#ifdef DEBUG
     printf("Unknown opcode: 0x%x\n", chip8->opcode);
+#endif
   }
 }
 
+void testSprite() {}
 /*
  * @Brief Returns the file size for a given file.
  */
@@ -190,7 +240,7 @@ long get_file_size(FILE *fp) {
   return size;
 }
 
-unsigned short getN(unsigned short opcode);
+unsigned short getN(unsigned short opcode) { return opcode & 0x000F; };
 unsigned short getNN(unsigned short opcode) { return opcode & 0x00FF; }
 unsigned short getNNN(unsigned short opcode);
 unsigned short getIndexX(unsigned short opcode) { return opcode >> 8 & 0x000F; }
